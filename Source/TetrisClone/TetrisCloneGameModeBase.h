32aec56bfbb252a6d2588c52e39f166d0277863a
Demarcus <dthecsguy@gmail.com>
2022-08-07 13:41:23 -0700

    second initial commit

---
+++ Source/TetrisClone/TetrisCloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TetrisCloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRISCLONE_API ATetrisCloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TetrisClone/hmm.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "hmm.h"
+
+// Sets default values
+Ahmm::Ahmm()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Ahmm::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	
+}
+
+// Called every frame
+void Ahmm::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
